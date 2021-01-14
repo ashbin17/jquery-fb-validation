d0c5689cdf29e0001c7eafc3269667878054ddf1
ashbin17 <ashbinmathew1997@gmail.com>
2021-01-14 14:52:48 +0530

    first commit

---
+++ jquery.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+    $("#signup").validate({
+        rules:{
+            fname:{
+                required:true,
+                minlength:4
+            },
+            sname:{
+                required:true,
+                minlength:4
+            },
+            mail:{
+                required:true,
+                email:true
+            },
+            password:{
+                required:true,
+                minlength:4,
+                maxlength:6
+            },
+            day:{
+                required:true
+            },
+            month:{
+                required:true
+            },
+            year:{
+                required:true
+            },
+            gender:{
+                required:true
+            }
+        },
+        messages:{
+            fname:{
+                required:"Enter first name",
+                minlength:"Enter atleast 4 characters"
+            }
+        }
+    })
+})
